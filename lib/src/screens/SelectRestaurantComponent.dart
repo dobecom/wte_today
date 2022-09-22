f30667176be59417b727af7db99b92b41766a0d7
dobecome <sylvius4@gmail.com>
2022-09-22 16:56:19 +0900

    식당 선택 시 다음 후보로 넘어가기

--- lib/src/screens/SelectRestaurantComponent.dart
+++ lib/src/screens/SelectRestaurantComponent.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/PlacesModel.dart';
@@ -5,48 +7,88 @@ import '../providers/LocationProvider.dart';
 import '../widgets/RestaurantPreviewWidget.dart';
 import 'package:wte_today/src/services/GglService.dart';
 
-class SelectRestaurantScreen extends StatelessWidget {
+class SelectRestaurantScreen extends StatefulWidget {
   const SelectRestaurantScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SelectRestaurantScreen> createState() => _SelectRestaurantScreenState();
+}
+
+class _SelectRestaurantScreenState extends State<SelectRestaurantScreen> {
   // final String url1 =
-  //     'https://postfiles.pstatic.net/MjAyMDAyMDFfMTU1/MDAxNTgwNTQzNTI3OTAz.XysiyRFf8pbVMvqoowWELBaoqRSmCIEet9AFc8rrFPog.uc4-vF47bJfIMLK9nUGJ1gQFJuxPXdq4nkiqp-uGVmog.JPEG.risingkeyword/IMG_3930.JPG?type=w966';
-  // final String url2 =
-  //     'https://postfiles.pstatic.net/MjAyMTEwMjBfOTUg/MDAxNjM0NzM3MzgxMjcw.PKhZZ72elrrKGcleogvrj5qxfLUu7OLLQ70wLaVRIjwg.ZzHELGBvJR6O1c4BlGo_d2xP5JEH3BnuvKB5f9f5EOAg.JPEG.joohj9404/output_2706785171.jpg?type=w773';
+  List<PlacesModel> selectedCurrentPlaces = [];
+
   @override
   Widget build(BuildContext context) {
     List<PlacesModel> currentPlaces =
         context.watch<LocationProvider>().currentPlaces;
     // GglService.getFullUrl('400', currentPlaces[0].photos[0].photoReference);
+    int count = 0;
 
     return Scaffold(
       body: Column(children: [
         Text('주변 식당 수 : ' + currentPlaces.length.toString()),
-        RestaurantPreviewWidget(
-          imageUrl: GglService.getFullUrl(
-              '400',
-              (currentPlaces[2].photos != null &&
-                      currentPlaces[2].photos.length > 0)
-                  ? currentPlaces[2].photos[0].photoReference
-                  : '' //Image(image: AssetImage('lib/images/noImageFound.png'))
-              // currentPlaces[0].photos[0].photoReference,
-              ),
-          restaurantName: currentPlaces[2].name,
-          ratings: currentPlaces[2].rating,
-          distance: currentPlaces[2].distance,
+        GestureDetector(
+          child: RestaurantPreviewWidget(
+            imageUrl: GglService.getFullUrl(
+                '400',
+                (currentPlaces[count].photos != null &&
+                        currentPlaces[count].photos.length > 0)
+                    ? currentPlaces[count].photos[0].photoReference
+                    : '' //Image(image: AssetImage('lib/images/noImageFound.png'))
+                // currentPlaces[0].photos[0].photoReference,
+                ),
+            restaurantName: currentPlaces[count].name,
+            ratings: currentPlaces[count].rating,
+            distance: currentPlaces[count].distance,
+          ),
+          onTap: () {
+            print('click first');
+            // currentPlaces.removeAt(2);
+            setState(() {
+              PlacesModel retVal = currentPlaces.removeAt(count);
+              selectedCurrentPlaces.add(retVal);
+              currentPlaces.removeAt(count + 1);
+              count = count + 2;
+            });
+          },
         ),
+
         const Divider(),
-        RestaurantPreviewWidget(
-          imageUrl: GglService.getFullUrl(
-              '400',
-              (currentPlaces[10].photos != null &&
-                      currentPlaces[10].photos.length > 0)
-                  ? currentPlaces[10].photos[0].photoReference
-                  : '' //Image(image: AssetImage('lib/images/noImageFound.png'))
-              // currentPlaces[1].photos[0].photoReference
-              ),
-          restaurantName: currentPlaces[10].name,
-          ratings: currentPlaces[10].rating,
-          distance: currentPlaces[10].distance,
+        GestureDetector(
+          child: RestaurantPreviewWidget(
+            imageUrl: GglService.getFullUrl(
+                '400',
+                (currentPlaces[count + 1].photos != null &&
+                        currentPlaces[count + 1].photos.length > 0)
+                    ? currentPlaces[count + 1].photos[0].photoReference
+                    : '' //Image(image: AssetImage('lib/images/noImageFound.png'))
+                // currentPlaces[1].photos[0].photoReference
+                ),
+            restaurantName: currentPlaces[count + 1].name,
+            ratings: currentPlaces[count + 1].rating,
+            distance: currentPlaces[count + 1].distance,
+          ),
+          onTap: () {
+            print('click second');
+            setState(() {
+              PlacesModel retVal = currentPlaces.removeAt(count + 1);
+              selectedCurrentPlaces.add(retVal);
+              currentPlaces.removeAt(count);
+              count = count + 2;
+            });
+          },
         ),
+        for (var restaurant in selectedCurrentPlaces) Text(restaurant.name)
+
+        // selectedCurrentPlaces.length > 0
+        //     ?
+        // Text(selectedCurrentPlaces[0].name)
+
+        // _getSelectedRestaurants()
+        //  (...selectedCurrentPlaces.map((e) => Text(e.name)).toList())
+        // selectedCurrentPlaces.map((e) => Text(e.name)).toList()
+        // : Container(child: Text('')),
         // RestaurantPreviewWidget(
         //   imageUrl: url1,
         //   restaurantName: '옛날 농장',
@@ -61,4 +103,10 @@ class SelectRestaurantScreen extends StatelessWidget {
       ]),
     );
   }
+
+// List<Widget> widgets = selectedCurrentPlaces.map((e) => Text(e.name)).toList();
+
+//   _getSelectedRestaurants() {
+//     return selectedCurrentPlaces.map((e) => {return Text(e.name)}).toList();
+//   }
 }
